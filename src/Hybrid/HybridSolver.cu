860f3890781f019a52daf7fc13c9d7665ce80b8c
Alexis Bon <alexisbon73@protonmail.com>
2026-01-06 17:14:10 +0100

    [wip]

--- src/Hybrid/HybridSolver.cu
+++ src/Hybrid/HybridSolver.cu
@@ -212,9 +212,9 @@ __global__ void deviceKernel(
 
 void HybridSolver::refillDeviceVertexQueue() {
     // set new divice vertex queue to old verticesUpdated
-    bool** tmp = &deviceVertexQueue;
+    bool* tmp = deviceVertexQueue;
     deviceVertexQueue = verticesUpdated;
-    verticesUpdated = *tmp;
+    verticesUpdated = tmp;
 
     // reset verticesUpdated
     memset(verticesUpdated, (int) false, (size_t) csr_graph->getNumberofVertices());
