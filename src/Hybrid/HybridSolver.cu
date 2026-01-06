6c9e1386516792322f8ff69da06d0193740e63b8
Alexis Bon <alexisbon73@protonmail.com>
2026-01-06 16:39:10 +0100

    [wip]

--- src/Hybrid/HybridSolver.cu
+++ src/Hybrid/HybridSolver.cu
@@ -94,6 +94,7 @@ uint HybridSolver::countVerticesInQueue() {
 
     for (int vertex = 0; vertex < csr_graph->getNumberofVertices(); vertex++) {
         if (verticesUpdated[vertex]) {
+            printf("  %d is now in queue");
             nbVertices++;
         }
     }
