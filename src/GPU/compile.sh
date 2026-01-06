5ab33c20eff6933eca3cd928a18dfde8397aaaf1
Alexis Bon <alexisbon73@protonmail.com>
2026-01-06 15:40:10 +0100

    Cleaning

--- src/GPU/compile.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/NVIDIA/cuCollections.git
-cd cuCollections && mkdir build && cd build && cmake .. && make -j
-cd cuCollections && sudo cp -r include/cuco /usr/local/include/
