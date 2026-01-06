23713974183c767b64a0e079ec862055379c40b4
Alexis Bon <alexisbon73@protonmail.com>
2026-01-06 15:40:54 +0100

    Add: HybridSolver launch on main program

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,23 @@
 #include <vector>
 #include <getopt.h>
 #include "CPUSolver.hpp"
+#include "GPUsolver.cuh"
+#include "HybridSolver.cuh"
 
 namespace {
 void print_usage(const char *prog) {
-    std::cerr << "Usage: " << prog << " -i <path/to/graph>\n"
-              << "       " << prog << " --input_graph <path/to/graph>\n";
+    std::cerr << "Usage: " << prog << " -i <path/to/graph> -v <CPU|GPU|Hybrid>\n"
+              << "       " << prog << " --input_graph <path/to/graph> --solver_version <CPU|GPU|Hybrid>\n";
 }
 } // namespace
 
 int main(int argc, char **argv) {
     std::string filename;
+    std::string solverVersion;
     const option long_opts[] = {
         {"input_graph", required_argument, nullptr, 'i'},
-        {"help", no_argument, nullptr, 'h'},
-        {nullptr, 0, nullptr, 0}};
+        {"solver_version", required_argument, nullptr, 'v'},
+        {"help", no_argument, nullptr, 'h'}};
 
     int opt;
     while ((opt = getopt_long(argc, argv, "i:h", long_opts, nullptr)) != -1) {
@@ -24,6 +27,8 @@ int main(int argc, char **argv) {
         case 'i':
             filename = optarg;
             break;
+        case 'v':
+            solverVersion = optarg;
         case 'h':
         default:
             print_usage(argv[0]);
@@ -31,13 +36,14 @@ int main(int argc, char **argv) {
         }
     }
 
-    if (filename.empty()) {
+    if (filename.empty() || solverVersion.empty()) {
         print_usage(argv[0]);
         return 1;
     }
 
-    CPUSolver solver(filename);
+    HybridSolver solver(filename);
     solver.solve(0);
-    solver.printResults();
+    solver.printDistances();
+    
     return 0;
 }
